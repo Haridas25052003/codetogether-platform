f8570b843523e67f92922c4832520639dc622fbd
Haridas Shinde <haridasshinde314@gmail.com>
2026-03-15 09:56:29 +0530

    "edited the controller class and added method"

--- src/main/java/com/demo/controller/CodeEditorController.java
+++ src/main/java/com/demo/controller/CodeEditorController.java
@@ -2,16 +2,42 @@ package com.demo.controller;
 
 import com.demo.model.CodeMessage;
 import org.springframework.messaging.handler.annotation.MessageMapping;
-import org.springframework.messaging.handler.annotation.SendTo;
 import org.springframework.stereotype.Controller;
+import org.springframework.messaging.simp.SimpMessagingTemplate;
 
 @Controller
 public class CodeEditorController {
 
-    @MessageMapping("/code-change")
-    @SendTo("/topic/editor")
-    public CodeMessage handleCodeChange(CodeMessage message) {
+    private final SimpMessagingTemplate messagingTemplate;
 
-        return message;
+    public CodeEditorController(SimpMessagingTemplate messagingTemplate) {
+        this.messagingTemplate = messagingTemplate;
+    }
+
+    @MessageMapping("/join")
+    public void joinRoom(CodeMessage message) {
+
+        messagingTemplate.convertAndSend(
+                "/topic/editor/" + message.getRoomId(),
+                message
+        );
+    }
+
+    @MessageMapping("/code-change")
+    public void handleCodeChange(CodeMessage message) {
+
+        messagingTemplate.convertAndSend(
+                "/topic/editor/" + message.getRoomId(),
+                message
+        );
+    }
+
+    @MessageMapping("/leave")
+    public void leaveRoom(CodeMessage message) {
+
+        messagingTemplate.convertAndSend(
+                "/topic/editor/" + message.getRoomId(),
+                message
+        );
     }
 }
